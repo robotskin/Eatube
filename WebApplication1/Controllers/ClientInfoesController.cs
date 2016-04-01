84d07a3adb36b8c6d4151f50b417f558795d9006
robotskin <robotskin@hotmail.com>
2016-04-01 11:36:49 +0100

    Test of DB branch

---
+++ WebApplication1/Controllers/ClientInfoesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ClientInfoesController : Controller
+    {
+        private ClientInfoContext db = new ClientInfoContext();
+
+        // GET: ClientInfoes
+        public ActionResult Index()
+        {
+            return View(db.ClientInfoes.ToList());
+        }
+
+        // GET: ClientInfoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ClientInfo clientInfo = db.ClientInfoes.Find(id);
+            if (clientInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(clientInfo);
+        }
+
+        // GET: ClientInfoes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ClientInfoes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "clientID,firstName,lastName")] ClientInfo clientInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ClientInfoes.Add(clientInfo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(clientInfo);
+        }
+
+        // GET: ClientInfoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ClientInfo clientInfo = db.ClientInfoes.Find(id);
+            if (clientInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(clientInfo);
+        }
+
+        // POST: ClientInfoes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "clientID,firstName,lastName")] ClientInfo clientInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(clientInfo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(clientInfo);
+        }
+
+        // GET: ClientInfoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ClientInfo clientInfo = db.ClientInfoes.Find(id);
+            if (clientInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(clientInfo);
+        }
+
+        // POST: ClientInfoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ClientInfo clientInfo = db.ClientInfoes.Find(id);
+            db.ClientInfoes.Remove(clientInfo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApplication1/Models/ClientModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class ClientInfo
+    {        
+        [Key]
+        public int clientID { get; set; }
+
+        [Required]
+        [Display(Name="First Name")]
+        public string firstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string lastName { get; set; }
+    }
+
+    public class OrderInfo
+    {
+        [Key]
+        public int orderID { get; set; }
+
+        public int clientID { get; set; }
+
+        public string orderDate { get; set; }
+
+        public virtual List<OrderItem> orderItems { get; set; }
+    }
+
+    public class OrderItem
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public int orderID { get; set; }
+
+        public int productID { get; set; }
+
+        public int quantity { get; set; }
+    }
+}
